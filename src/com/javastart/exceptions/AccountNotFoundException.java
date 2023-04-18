8e657dc324935492d5ed01f4aedd3d97906788e4
Antoshkin Igor <antoshkinigor@hotmail.com>
2023-04-18 18:09:10 +0300

    ыть

--- src/com/javastart/exceptions/AccountNotFoundException.java
+++ src/com/javastart/exceptions/AccountNotFoundException.java
@@ -1,5 +1,7 @@
 package com.javastart.exceptions;
 
 public class AccountNotFoundException extends Exception {
-
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/com/javastart/exceptions/Main.java
+++ src/com/javastart/exceptions/Main.java
@@ -1,7 +1,28 @@
 package com.javastart.exceptions;
 
-public class Main {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+
+    public static void main(String[] args) {
+        //List <Account> accountList = new ArrayList<>();
+        AccountRepository accountRepository = AccountRepository.getInstance();
+        //AccountRepository.getInstance();
+        Account account = new Account(1L,"Lori",10000);
+        Account account1 = new Account(2L,"Lori",20000);
+        Account account2 = new Account(3L,"Lori",30000);
+        System.out.println(accountRepository);
+
+        accountRepository.add(account);
+        accountRepository.add(account1);
+        accountRepository.add(account2);
+        System.out.println(accountRepository);
+        try {
+            accountRepository.getById(2L);
+        } catch (AccountNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
